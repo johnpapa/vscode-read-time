c5fd5321629369ac1ebda994cadbfc573c3c7c6d
John Papa <john@johnpapa.net>
2019-04-11 20:24:31 -0400

    add new settings to setup and teardown

--- src/test/lib/setup-teardown-test-suite.ts
+++ src/test/lib/setup-teardown-test-suite.ts
@@ -1,5 +1,12 @@
 import { Commands, getExtension, IReadTimeSettings } from '../../models';
-import { getEnabledSetting, updateEnabledSetting } from '../../configuration';
+import {
+  getEnabledSetting,
+  updateEnabledSetting,
+  getFileTypesSetting,
+  getWPMSetting,
+  updateFileTypesSetting,
+  updateWPMSetting
+} from '../../configuration';
 import * as vscode from 'vscode';
 
 export const executeCommand = vscode.commands.executeCommand;
@@ -21,12 +28,18 @@ export async function setupTestSuite(
   let extension = getExtension();
   // Save the original values
   originalValues.enabled = getEnabledSetting();
+  originalValues.fileTypes = getFileTypesSetting();
+  originalValues.wordsPerMinute = getWPMSetting();
   // Set the test values
   await updateEnabledSetting(true);
+  await updateFileTypesSetting(['markdown']);
+  await updateWPMSetting(200);
   return extension;
 }
 
 export async function teardownTestSuite(originalValues: IReadTimeSettings) {
   // put back the original peacock user settings
   await updateEnabledSetting(originalValues.enabled);
+  await updateFileTypesSetting(originalValues.fileTypes);
+  await updateWPMSetting(originalValues.wordsPerMinute);
 }
